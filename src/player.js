b7fb697ef5f86ec765f99a9ec56773c841b29640
Cody Baldwin <cbald24@win.cs.ksu.edu>
2016-09-20 19:51:08 -0500

    worked a little added a background image

--- src/player.js
+++ src/player.js
@@ -1,19 +1,19 @@
 "use strict";
 
 const MS_PER_FRAME = 1000/8;
-
+var moveSpeed = 200
 /**
  * @module exports the Player class
  */
 module.exports = exports = Player;
-
+var moveDistance = 0;
 /**
  * @constructor Player
  * Creates a new player object
  * @param {Postition} position object specifying an x and y
  */
 function Player(position) {
-  this.state = "idle";
+  this.state = "hopRight";
   this.x = position.x;
   this.y = position.y;
   this.width  = 64;
@@ -22,6 +22,7 @@ function Player(position) {
   this.spritesheet.src = encodeURI('assets/PlayerSprite2.png');
   this.timer = 0;
   this.frame = 0;
+  thi
 }
 
 /**
@@ -38,6 +39,16 @@ Player.prototype.update = function(time) {
         if(this.frame > 3) this.frame = 0;
       }
       break;
+    case "hopRight":
+      this.timer += time;
+      this.x += (moveSpeed*time);
+      moveDistance -= (moveSpeed*time);
+      if (moveDistance <= 0) this.state = "idle";
+      if(this.timer > MS_PER_FRAME) {
+        this.timer = 0;
+        this.frame += 1;
+        if(this.frame > 3) this.frame = 0;
+      }
     // TODO: Implement your player's update by state
   }
 }
@@ -61,4 +72,11 @@ Player.prototype.render = function(time, ctx) {
       break;
     // TODO: Implement your player's redering according to state
   }
+}
+
+Player.prototype.moveRight = function(){
+  this.state = "hopRight";
+  this.frame = 0;
+  moveDistance = this.width;
+
 }
